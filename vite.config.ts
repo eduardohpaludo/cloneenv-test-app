5d9dd0e3190009c5f9320ef8ed790f887908534e
eduardohpaludo <97770245+eduardohpaludo@users.noreply.github.com>
2025-11-26 11:25:29 -0300

    Move server config to preview section

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', // Permite acesso externo ao container
-    allowedHosts: true, // ⚠️ Libera qualquer host (ideal para previews dinâmicos)
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['all'], // Libera geral
   },
   plugins: [react()],
 })
